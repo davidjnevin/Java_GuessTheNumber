fc8894fbc86c497bd910010b5d26021049ed02f0
David Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-07-04 21:58:56 +0200

    Functioning well.

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -1,1 +1,46 @@
-public 
+import java.util.Scanner;
+
+class main {
+
+    public static void main(String[] args) {
+        {
+            int maxLimit = 100;
+            int minLimit = 0;
+            int maxAttempts = 6;
+            int secret_number = (int) (Math.random() * (maxLimit - minLimit + 1));
+            int userGuessNumber = 0;
+
+            System.out.println(
+                    "You are playing a game: Guess the Number. \nYou have a maximum of " + maxAttempts
+                            + " attempts to guess the number. \n ");
+
+            for (int attempt = 0; attempt < maxAttempts; attempt++) {
+                userGuessNumber = takeInput(minLimit, maxLimit, userGuessNumber);
+                String guessesLeft = "You have " + (maxAttempts - attempt - 1) + " attempts remaining.";
+
+                if (userGuessNumber < secret_number) {
+                    System.out.println("You guessed too small.\n" + guessesLeft);
+                } else if (userGuessNumber > secret_number) {
+                    System.out.println("You guessed too high.\n" + guessesLeft);
+                } else {
+                    System.out.println("That it! You got it!");
+                    return;
+                }
+            }
+            System.out.println("The number was " + secret_number + ".\n");
+
+        }
+    }
+
+    public static int takeInput(int minLimit, int maxLimit, int guess) {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Please enter a number between " + minLimit + " and " + maxLimit + ".\n");
+        if (userInput.hasNextInt()) {
+            guess = userInput.nextInt();
+        } else {
+            System.out.println("Please enter a number between " + minLimit + " and " + maxLimit + ".\n");
+        }
+        return guess;
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Guess the Number
+
+A basic CLI game written in Java.
+
+(Just getting my feet wet.)
+
+* User has a limit number of guesses.
+* user input is checked for validity
+* user is informed if they guess correctly with the limit of guesses.
